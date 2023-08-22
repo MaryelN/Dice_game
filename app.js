e664b7b7592e835b3421d8fce5959463fe94f610
Maryel Navarro <maryel.navarromtz@gmai.com>
2023-08-22 17:11:33 +0200

    add change of player on function & .active class

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 
 //Buttons
-const dice = document.querySelector(".dice");
+const dice = document.getElementById("dice");
 
-const btnRoll = document.querySelector(".roll");
+const btnRoll = document.getElementById("roll");
 
 const btnHold = document.getElementById("hold");
 
@@ -16,15 +16,13 @@ const player1 = document.getElementById("score-1");
 
 const player2 = document.getElementById("score-2");
 
-const activePlayer = document.querySelector(".player-1");
-
-const activePlayer2 = document.querySelector(".player-2");
-
+let activePlayer = 1;
 let currentScore = 0;
 
 btnRoll.addEventListener("click", function() {
-    
-    activePlayer.classList.add("active");
+  //Change the active player  
+    const activePlayerClass = document.getElementById(`current-${activePlayer}`);
+    activePlayerClass.classList.add("active");
 
   //Random number inside the fuction so it changes everytime the button is clicked
   const randomNum = Math.floor(Math.random() * 6 ) + 1;
@@ -37,12 +35,21 @@ btnRoll.addEventListener("click", function() {
   //Add and display the random number if !== 1
   if (randomNum !== 1) {
     currentScore += randomNum;
-    currentPlayer1.innerHTML = currentScore;
+
+    // currentPlayer1.innerHTML = currentScore on html;
+    document.getElementById(`current-${activePlayer}`).innerHTML = currentScore;
 
   } else {
+
     currentScore = 0;
-    currentPlayer1.innerHTML = currentScore;
-    activePlayer.classList.remove("active");
+    //set the current score to 0 on html
+    document.getElementById(`current-${activePlayer}`).innerHTML = currentScore;
+    activePlayerClass.classList.remove("active");
+    //change of player
+
+    activePlayer = activePlayer === 1 ? 2 : 1;
+    document.getElementById(`current-${activePlayer}`).classList.add("active");
+    // activePlayer2.classList.add("active");
   }
     
 });
